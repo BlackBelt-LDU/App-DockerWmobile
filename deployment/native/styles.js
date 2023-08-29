d3566b607c23733958aea75217daa6cb99fc47cf
Attila Szabo <attila.aszabo@blackbelt.hu>
2023-08-29 17:19:00 +0200

    add menidx init

---
+++ deployment/native/styles.js
@@ -0,0 +1,11 @@
+import * as style0 from "C:/Users/Attila/Documents/Mendix/App-DockerWmobile/themesource/administration/native/main";
+import * as style1 from "C:/Users/Attila/Documents/Mendix/App-DockerWmobile/themesource/nanoflowcommons/native/main";
+import * as style2 from "C:/Users/Attila/Documents/Mendix/App-DockerWmobile/themesource/webactions/native/main";
+import * as style3 from "C:/Users/Attila/Documents/Mendix/App-DockerWmobile/themesource/atlas_core/native/main";
+import * as style4 from "C:/Users/Attila/Documents/Mendix/App-DockerWmobile/themesource/feedbackmodule/native/main";
+import * as style5 from "C:/Users/Attila/Documents/Mendix/App-DockerWmobile/themesource/myfirstmodule/native/main";
+import * as style6 from "C:/Users/Attila/Documents/Mendix/App-DockerWmobile/theme/native/main";
+
+import { flatten } from "mendix/native";
+
+module.exports = flatten([style0, style1, style2, style3, style4, style5, style6]);
